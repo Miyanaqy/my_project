6215188d2d5e43b06c1c2a5945e2592f213d5e3c
haoniansheng <haoniansheng1@163.com>
2017-02-04 13:37:34 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,17 +8,24 @@ var container_down={
 		this.$ul=$('.container_item');
 		this.$li.on("mouseenter",e=>{
     		e.preventDefault();
-    		this.$container.addClass('in');
-    		this.$ul.addClass('collapsing');
+    		// this.$container.addClass('in');
+    		// this.$ul.addClass('collapsing');
+
+
+    	 
+			// $(e.target).find('.container').addClass('in');
+			$(e.target).find('.container_item').addClass('collapsing');
+		 
+
+
     	});
     	this.$li.on("mouseleave",e=>{
     		e.preventDefault();
-    		this.$container.removeClass('in');
+    		// this.$container.removeClass('in');
     		this.$ul.removeClass('collapsing');
     	});
 	 
 	}
 }
 container_down.init();
-
  
