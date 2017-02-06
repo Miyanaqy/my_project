46400b032c30aa0a5370002f2c614ecafb7a29ce
haoniansheng <haoniansheng1@163.com>
2017-02-06 13:42:15 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,27 +2,51 @@ var container_down={
 	$container:null,
 	$li:null,
 	$ul:null,
+	timer:null,
+	timer2:null,
+	height:null,
 	init(){
 		this.$li=$('.nav-item');
 		this.$container=$('.container');
 		this.$ul=$('.container_item');
 		this.$li.on("mouseenter",e=>{
-    		e.preventDefault();
+		 
     		// this.$container.addClass('in');
     		// this.$ul.addClass('collapsing');
-
-
-    	 
 			// $(e.target).find('.container').addClass('in');
-			$(e.target).find('.container_item').addClass('collapsing');
-		 
-
-
+			$(e.target).parent().find('.container_item').addClass('collapsing');
+			this.height=0;
+			this.timer=setInterval(()=> {
+				$(e.target).parent().find('.container_item')
+				.css({
+					"height":this.height
+				})
+				this.height+=10;
+				if (this.height>=210) {
+					this.height=210;
+					clearTimeout(this.timer); 
+	        		this.timer=null;
+				}
+			},10);
     	});
     	this.$li.on("mouseleave",e=>{
+    		 
     		e.preventDefault();
     		// this.$container.removeClass('in');
-    		this.$ul.removeClass('collapsing');
+    		this.height=210;
+    		this.timer2=setInterval(()=> {
+				$('.container_item')
+				.css({
+					"height":this.height
+				})
+				this.height-=10;
+				if (this.height<=0) {
+					this.height=0;
+					clearTimeout(this.timer2); 
+	        		this.timer2=null;
+	        		this.$ul.removeClass('collapsing');
+				}
+			},10);
     	});
 	 
 	}
